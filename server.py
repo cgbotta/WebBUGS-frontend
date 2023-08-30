815f6853f5ce69044fe43923eb906c386b7eeca4
Colton Botta <39240537+cgbotta@users.noreply.github.com>
2023-08-30 13:42:27 -0400

    Update plots to plotly

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 import bs4
 import os
+from statistics import mean
+from scipy.stats import norm
 
 app = Flask(__name__)
 app.secret_key = "3d6f45a5fc12445dbac2f59c3b6c7cb1"
@@ -23,7 +25,7 @@ session.mount('https://', adapter)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', samples="")
 
 
 @app.route('/graphs_page')
@@ -79,13 +81,16 @@ def my_link():
   r = None
   feedback = ""
   try:
-    r = requests.get(url='https://flask-service.a4b97h85mfgc0.us-east-2.cs.amazonlightsail.com/compile/', data ={'user_input':user_input, 'data_input':user_data, 'inits_input':inits_input, 'monitors_input':monitors_input})
-    # r = requests.get(url='http://127.0.0.1:3000/compile/', data ={'user_input':user_input, 'data_input':user_data, 'inits_input':inits_input, 'monitors_input':monitors_input})
+    # r = requests.get(url='https://flask-service.a4b97h85mfgc0.us-east-2.cs.amazonlightsail.com/compile/', data ={'user_input':user_input, 'data_input':user_data, 'inits_input':inits_input, 'monitors_input':monitors_input})
+    r = requests.get(url='http://127.0.0.1:3000/compile/', data ={'user_input':user_input, 'data_input':user_data, 'inits_input':inits_input, 'monitors_input':monitors_input})
     body = r.json()
     feedback = body["logs"]
-    data = body['data']
+    data = body["data"]
+    print(data)
     # print(data)
-    print(len(data))
+    samples = {}
+    # print(data)
+    # print(len(data))
     sub = 'line ' + str(num_lines + 1)
     if sub in feedback:
       feedback = feedback + 'You may need to add a closing bracket to your model\n------\nmodel{\n.\n}'
@@ -101,20 +106,44 @@ def my_link():
     images = []
     for d in data:
       plt.figure()
-      plt.xlabel('value')
-      plt.ylabel("samples")
+      plt.xlabel('value of ' + d[0])
+      plt.ylabel("probability")
+      plt.yticks([]) 
       plt.title(d[0])
-      # plt.hist(d[1], bins=20)
-      plt.plot(range(len(d[1])), d[1])
+      height, _, _ = plt.hist(d[1], bins=20)
+      plt.axvline(mean(d[1]), color='k', linestyle='dashed', linewidth=1)
+
+      min_ylim, max_ylim = plt.ylim()
+      plt.text(mean(d[1]), max_ylim*0.9, 'Mean: {:.3f}'.format(mean(d[1])))
+
+      ci = norm(*norm.fit(d[1])).interval(0.95)  # fit a normal distribution and get 95% c.i.
+      plt.fill_betweenx([0, height.max()], ci[0], ci[1], color='g', alpha=0.1)  # Mark between 0 and the highest bar in the histogram
       filename = "./static/images/output_" + d[0] + ".jpg"
       plt.savefig(filename)
       plt.close()
-      images.append("https://cgb45.pythonanywhere.com/static/images/output_" + d[0] + ".jpg")
-      # images.append("http://127.0.0.1:8080/static/images/output_" + d[0] + ".jpg")
+      # images.append("https://cgb45.pythonanywhere.com/static/images/output_" + d[0] + ".jpg")
+      images.append("http://127.0.0.1:8080/static/images/output_" + d[0] + ".jpg")
+
+      plt.clf()
+
+      # graph 2
+      plt.figure()
+      plt.xlabel('sample')
+      plt.ylabel("value")
+      plt.title(d[0])
+      ax1 = plt.plot(range(10000), d[1], 'o')
+
+      filename = "./static/images/output_samples_" + d[0] + ".jpg"
+      plt.savefig(filename)
+      plt.close()
+      # images.append("https://cgb45.pythonanywhere.com/static/images/output_" + d[0] + ".jpg")
+      images.append("http://127.0.0.1:8080/static/images/output_samples_" + d[0] + ".jpg")
+
+      plt.clf()
 
 
-    # x = range(10000)
-    # y = data[1][1]
+      # samples
+      samples[d[0]] = d[1]
 
     # fig = plt.figure()
     # plt.xlim(0, 10000)
@@ -135,7 +164,7 @@ def my_link():
     # ani.save("./static/TLI.gif", dpi=300, writer=PillowWriter(fps=1))
 
 
-    plt.clf()
+
 
     # load the file
     # if os.path.isfile("./templates/graphs_page.html"):
@@ -158,19 +187,38 @@ def my_link():
   # return redirect("/")
 
 
-
+  print(samples)
   if len(images) == 0:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback)
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback)
   elif len(images) == 1:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0])
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0])
   elif len(images) == 2:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0], IMAGE2 = images[1])
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1])
   elif len(images) == 3:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0], IMAGE2 = images[1], IMAGE3 = images[2])
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2])
   elif len(images) == 4:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0], IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3])
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                           IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3])
+  elif len(images) == 5:
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4])
+  elif len(images) == 6:
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4], IMAGE6 = images[5])
+  elif len(images) == 7:
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4], IMAGE6 = images[5], IMAGE7 = images[6])
+  elif len(images) == 8:
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4], IMAGE6 = images[5], IMAGE7 = images[6], IMAGE8 = images[7])
+  elif len(images) == 9:
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4], IMAGE6 = images[5], IMAGE7 = images[6], IMAGE8 = images[7], IMAGE9 = images[8])
   else:
-    return render_template("index.html", BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0], IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3])
+    return render_template("index.html", samples=samples, BUGS_CODE = user_input, DATA_INPUT = user_data,  INITS_INPUT = inits_input,  MONITORS_INPUT = monitors_input, FEEDBACK = feedback, IMAGE1 = images[0],
+                            IMAGE2 = images[1], IMAGE3 = images[2], IMAGE4 = images[3], IMAGE5 = images[4], IMAGE6 = images[5], IMAGE7 = images[6], IMAGE8 = images[7], IMAGE9 = images[8], IMAGE10 = images[9])
 
   # Handle errors version 
 
@@ -190,17 +238,17 @@ def my_link():
   bugs_data = translate_data(user_data)
   return render_template("index.html", BUGS_CODE_TEXTBOX = bugs_code, BUGS_CODE = user_input, DATA_INPUT = user_data, BUGS_DATA_TEXTBOX = bugs_data)  
 
-@app.route('/aws/', methods=["POST"])
-def aws():
-  r = None
-  dict = {}
-  try:
-    # r = session.get('https://flask-service.a4b97h85mfgc0.us-east-2.cs.amazonlightsail.com/please')
-    r = session.get('http://127.0.0.1:3000/please')
+# @app.route('/aws/', methods=["POST"])
+# def aws():
+#   r = None
+#   dict = {}
+#   try:
+#     # r = session.get('https://flask-service.a4b97h85mfgc0.us-east-2.cs.amazonlightsail.com/please')
+#     r = session.get('http://127.0.0.1:3000/please')
 
-  except Exception as e:
-    print("error: ", e)
-  return render_template("index.html", BUGS_CODE = r.text)
+#   except Exception as e:
+#     print("error: ", e)
+#   return render_template("index.html", BUGS_CODE = r.text)
 
 @app.route('/clear_data/', methods=["POST"])
 def clear_data():
@@ -238,7 +286,7 @@ def get_example(id):
   except Exception as e:
     print("error: ", e)
   
-  return render_template("index.html", BUGS_CODE = code_file_contents, DATA_INPUT = data_file_contents, MONITORS_INPUT = monitors_file_contents, INITS_INPUT = inits_file_contents)
+  return render_template("index.html", samples="", BUGS_CODE = code_file_contents, DATA_INPUT = data_file_contents, MONITORS_INPUT = monitors_file_contents, INITS_INPUT = inits_file_contents)
 
 
 if __name__ == '__main__':
